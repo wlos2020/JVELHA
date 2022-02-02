96f0e447fff83919c27b969e09f6ba546490e963
Wlos <wlos.ba@gmail.com>
2022-02-02 01:35:50 -0300

    Adicionar arquivos de projeto.

---
+++ JVELHA/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace JVELHA
+{
+    partial class JVelha
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BL1C1 = new System.Windows.Forms.Button();
+            this.BL1C2 = new System.Windows.Forms.Button();
+            this.BL1C3 = new System.Windows.Forms.Button();
+            this.BL2C3 = new System.Windows.Forms.Button();
+            this.BL2C2 = new System.Windows.Forms.Button();
+            this.BL2C1 = new System.Windows.Forms.Button();
+            this.BL3C3 = new System.Windows.Forms.Button();
+            this.BL3C2 = new System.Windows.Forms.Button();
+            this.BL3C1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // BL1C1
+            // 
+            this.BL1C1.Location = new System.Drawing.Point(24, 35);
+            this.BL1C1.Name = "BL1C1";
+            this.BL1C1.Size = new System.Drawing.Size(50, 50);
+            this.BL1C1.TabIndex = 0;
+            this.BL1C1.UseVisualStyleBackColor = true;
+            this.BL1C1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // BL1C2
+            // 
+            this.BL1C2.Location = new System.Drawing.Point(80, 35);
+            this.BL1C2.Name = "BL1C2";
+            this.BL1C2.Size = new System.Drawing.Size(50, 50);
+            this.BL1C2.TabIndex = 1;
+            this.BL1C2.UseVisualStyleBackColor = true;
+            // 
+            // BL1C3
+            // 
+            this.BL1C3.Location = new System.Drawing.Point(136, 35);
+            this.BL1C3.Name = "BL1C3";
+            this.BL1C3.Size = new System.Drawing.Size(50, 50);
+            this.BL1C3.TabIndex = 2;
+            this.BL1C3.UseVisualStyleBackColor = true;
+            // 
+            // BL2C3
+            // 
+            this.BL2C3.Location = new System.Drawing.Point(136, 91);
+            this.BL2C3.Name = "BL2C3";
+            this.BL2C3.Size = new System.Drawing.Size(50, 50);
+            this.BL2C3.TabIndex = 5;
+            this.BL2C3.UseVisualStyleBackColor = true;
+            // 
+            // BL2C2
+            // 
+            this.BL2C2.Location = new System.Drawing.Point(80, 91);
+            this.BL2C2.Name = "BL2C2";
+            this.BL2C2.Size = new System.Drawing.Size(50, 50);
+            this.BL2C2.TabIndex = 4;
+            this.BL2C2.UseVisualStyleBackColor = true;
+            // 
+            // BL2C1
+            // 
+            this.BL2C1.Location = new System.Drawing.Point(24, 91);
+            this.BL2C1.Name = "BL2C1";
+            this.BL2C1.Size = new System.Drawing.Size(50, 50);
+            this.BL2C1.TabIndex = 3;
+            this.BL2C1.UseVisualStyleBackColor = true;
+            // 
+            // BL3C3
+            // 
+            this.BL3C3.Location = new System.Drawing.Point(136, 147);
+            this.BL3C3.Name = "BL3C3";
+            this.BL3C3.Size = new System.Drawing.Size(50, 50);
+            this.BL3C3.TabIndex = 8;
+            this.BL3C3.UseVisualStyleBackColor = true;
+            // 
+            // BL3C2
+            // 
+            this.BL3C2.Location = new System.Drawing.Point(80, 147);
+            this.BL3C2.Name = "BL3C2";
+            this.BL3C2.Size = new System.Drawing.Size(50, 50);
+            this.BL3C2.TabIndex = 7;
+            this.BL3C2.UseVisualStyleBackColor = true;
+            // 
+            // BL3C1
+            // 
+            this.BL3C1.Location = new System.Drawing.Point(24, 147);
+            this.BL3C1.Name = "BL3C1";
+            this.BL3C1.Size = new System.Drawing.Size(50, 50);
+            this.BL3C1.TabIndex = 6;
+            this.BL3C1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(32, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(154, 23);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "X é a sua vez";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // JVelha
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(209, 215);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BL3C3);
+            this.Controls.Add(this.BL3C2);
+            this.Controls.Add(this.BL3C1);
+            this.Controls.Add(this.BL2C3);
+            this.Controls.Add(this.BL2C2);
+            this.Controls.Add(this.BL2C1);
+            this.Controls.Add(this.BL1C3);
+            this.Controls.Add(this.BL1C2);
+            this.Controls.Add(this.BL1C1);
+            this.Name = "JVelha";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.JVelha_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BL1C1;
+        private System.Windows.Forms.Button BL1C2;
+        private System.Windows.Forms.Button BL1C3;
+        private System.Windows.Forms.Button BL2C3;
+        private System.Windows.Forms.Button BL2C2;
+        private System.Windows.Forms.Button BL2C1;
+        private System.Windows.Forms.Button BL3C3;
+        private System.Windows.Forms.Button BL3C2;
+        private System.Windows.Forms.Button BL3C1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ JVELHA/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JVELHA
+{
+    public partial class JVelha : Form
+    {
+        bool xis = true;
+        public JVelha()
+        {
+            InitializeComponent();
+        }
+
+        private void JVelha_Load(object sender, EventArgs e)
+        {
+
+            BL1C1.Click += new EventHandler(BClick);
+            BL1C2.Click += new EventHandler(BClick);
+            BL1C3.Click += new EventHandler(BClick);
+            BL2C1.Click += new EventHandler(BClick);
+            BL2C2.Click += new EventHandler(BClick);
+            BL2C3.Click += new EventHandler(BClick);
+            BL3C1.Click += new EventHandler(BClick);
+            BL3C2.Click += new EventHandler(BClick);
+            BL3C3.Click += new EventHandler(BClick);
+
+            foreach (Control item in this.Controls)
+            {
+                if (item is Button)
+                {
+                    item.TabStop = false;
+                }
+            }
+        }
+        private void BClick(object sender, EventArgs e)
+        {
+            ((Button)sender).Text = this.xis ? "x" : "o";
+            ((Button)sender).Enabled = false;
+            VerificarGanhador();
+            xis = !xis;
+            label1.Text = String.Format("{0}, É a sua vez", this.xis ? "x" : "o");
+
+        }
+        private void VerificarGanhador()
+        {
+            if (
+                    BL1C1.Text != String.Empty && BL1C1.Text == BL1C2.Text && BL1C2.Text == BL1C3.Text || //linha 1
+                    BL2C1.Text != String.Empty && BL2C1.Text == BL2C2.Text && BL2C2.Text == BL2C3.Text || //linha 2
+                    BL3C1.Text != String.Empty && BL3C1.Text == BL3C2.Text && BL3C2.Text == BL3C3.Text || //linha 3
+
+                    BL1C1.Text != String.Empty && BL1C1.Text == BL2C1.Text && BL2C1.Text == BL3C1.Text || //COluna 1
+                    BL1C2.Text != String.Empty && BL1C2.Text == BL2C2.Text && BL2C2.Text == BL3C2.Text || //Coluna 2
+                    BL1C3.Text != String.Empty && BL1C3.Text == BL2C3.Text && BL2C3.Text == BL3C3.Text || //Coluna 3
+
+                    BL1C1.Text != String.Empty && BL1C1.Text == BL2C2.Text && BL2C2.Text == BL3C3.Text || // Diagonal\
+                    BL1C3.Text != String.Empty && BL1C3.Text == BL2C2.Text && BL2C2.Text == BL3C1.Text // Diagonal\
+                )
+            {
+                MessageBox.Show(String.Format("O ganhador é o {0}", xis ? "x" : "o"), "Temos um Vencedor", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                Reiniciar();
+            }
+            else
+            {
+                VerificarEmpate();
+            }
+
+        }
+        private void VerificarEmpate()
+        {
+            bool todosDesabilitados = true;
+
+            foreach (Control item in this.Controls)
+            {
+                if (item is Button && item.Enabled)
+                {
+                    todosDesabilitados = false;
+                    break;
+                }
+            }
+
+            if (todosDesabilitados)
+            {
+                MessageBox.Show(String.Format("Deu empate"), "Ops!!!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+                Reiniciar();
+            }
+        }
+        private void Reiniciar()
+        {
+            foreach (Control item in this.Controls)
+            {
+                if (item is Button)
+                {
+                    item.Enabled = true;
+                    item.Text = String.Empty;
+                }
+            }
+        }
+    }
+
+}
